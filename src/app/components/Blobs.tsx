528067b825649b6a49690234b2da653161aad887
Byte Development <133065477+Byte-Developments@users.noreply.github.com>
2025-03-30 21:33:20 -0400

    Fix CTX

--- src/app/components/Blobs.tsx
+++ src/app/components/Blobs.tsx
@@ -9,9 +9,10 @@ export default function DiagonalStripesBackground() {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
+    const context = canvas.getContext("2d");
+    if (!context) return;
 
+    const ctx = context as CanvasRenderingContext2D;
     let animationFrameId: number;
 
     canvas.width = window.innerWidth;
