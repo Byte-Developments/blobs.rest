c0bb60b4c45f17ea71cfb18376c12ca4aff1d7f1
Byte Development <133065477+Byte-Developments@users.noreply.github.com>
2025-04-09 16:27:04 -0400

    Change text font

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Sofia: ['Sofia Pro Regular', 'serif'],
+        SofiaBold: ['Sofia Pro Bold', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
