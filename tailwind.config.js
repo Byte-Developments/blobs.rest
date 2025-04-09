f129639fd83bf77214210b522bf48106e8f32c1e
Byte Development <133065477+Byte-Developments@users.noreply.github.com>
2025-04-09 16:50:00 -0400

    Add font to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'SofiaPro': ['sofia-pro', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
